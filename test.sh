b7cfa4fd467edd12b2ea938a508cef4a64218c3f
Nick Baker <ndbaker1@outlook.com>
2022-04-19 23:27:00 -0500

    use test scenario where TCs are required

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 echo "0 UP 0 1
 0 UP 1 0
-0 UP 0 2
-0 UP 2 0
 0 UP 1 2
-0 UP 2 1" > topology.txt
+0 UP 2 1
+0 UP 2 3
+0 UP 3 2" > topology.txt
 
-python node.py 0 0 &
-python node.py 1 2 "sending from 1 to 2" 20 &
+python node.py 0 2 "sending from 0 to 2" 20 &
+python node.py 1 1 &
 python node.py 2 2 &
+python node.py 3 3 &
 python controller.py &
